dde948c7b356a3f4cb72b1320f314150109bcec6
Ingris Andrade <iac22@outlook.com>
2022-03-21 19:56:32 -0300

    Adiciona evento que clica em uma das cores da paleta e faz com que ela seja selecionada e utilizada para preencher os pixels no quadro

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function addFrame() {
   }
 }
 addFrame();
+
+const cor1 = document.querySelector('#cor1');
+const cor2 = document.querySelector('#cor2');
+const cor3 = document.querySelector('#cor3');
+const cor4 = document.querySelector('#cor4');
+const input = document.querySelector('#input');
+
+function changeSelected(event) {
+  const changeElement = document.querySelector('.selected');
+  changeElement.classList.remove('selected');
+  event.target.classList.add('selected');
+  input.value = '';
+}
+
+cor1.addEventListener('click', changeSelected);
+cor2.addEventListener('click', changeSelected);
+cor3.addEventListener('click', changeSelected);
+cor4.addEventListener('click', changeSelected);
